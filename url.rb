63144d87031234cd7b3fe984e490c91ed9b916a0
taman9333 <abdelrahman.taman@robustastudio.com>
2019-12-07 21:43:30 +0200

    Create url shortcode * Add shorten post action to create url shortcode * Add shorten url service to handle creation & errors of creating shorten url * Add model validations

--- url.rb
+++ url.rb
@@ -1,5 +1,7 @@
 require 'sinatra/activerecord'
 
 class Url < ActiveRecord::Base
-
+  validates :url, presence: true
+  validates :shortcode, presence: true, uniqueness: true,
+                        format: { with: /\A[0-9a-zA-Z_]{4,}\z/i }
 end
