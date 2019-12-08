09418f791fd386e82e3e3af13a997812fb5fe6f5
taman9333 <abdelrahman.taman@robustastudio.com>
2019-12-08 05:04:45 +0200

    Handle if shortcode is not present in params * Add generate unique shortcode with checking database * Add create unique url record with rescue & retrying to handle not unique records exception in case of race conditions * Add CHARSETS which should be matched with randomly generated shortcode

--- url.rb
+++ url.rb
@@ -1,7 +1,18 @@
 require 'sinatra/activerecord'
 
 class Url < ActiveRecord::Base
+  CHARSETS = ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a + ['_']
+
   validates :url, presence: true
   validates :shortcode, presence: true, uniqueness: true,
                         format: { with: /\A[0-9a-zA-Z_]{4,}\z/i }
+
+  def self.generate_unique_shortcode
+    unique_shortcode = nil
+    loop do
+      unique_shortcode = 6.times.map { CHARSETS.sample }.join
+      break if Url.find_by_shortcode(unique_shortcode).nil?
+    end
+    unique_shortcode
+  end
 end
